26863bafab630483b088d41103711b072d098f76
Zak Franczak <zakfranczak@gmail.com>
2023-07-21 12:17:54 -0500

    buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calculator from './components/Calculator';
-
-const calcButtons = [
-  {
-    keyCode: '',
-    key: '',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '/',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '*',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '7',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '8',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '9',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '-',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '4',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '5',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '6',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '+',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '1',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '2',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '3',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '=',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '0',
-    id: '',
-  },
-  {
-    keyCode: '',
-    key: '.',
-    id: '',
-  },
-];
+import './App.css';
 
 const App = () => {
-  return <Calculator />;
+  return (
+    <div className='container'>
+      <Calculator />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,127 @@
 import React from 'react';
+import '../styles/calculator.css';
+
+const calcButtons = [
+  {
+    keyCode: '',
+    key: 'AC',
+    id: '',
+    class: 'all-clear',
+  },
+  {
+    keyCode: '',
+    key: '/',
+    id: '',
+    class: 'operator button',
+  },
+  {
+    keyCode: '',
+    key: '*',
+    id: '',
+    class: 'operator button',
+  },
+  {
+    keyCode: '',
+    key: '7',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '8',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '9',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '-',
+    id: '',
+    class: 'operator button',
+  },
+  {
+    keyCode: '',
+    key: '4',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '5',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '6',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '+',
+    id: '',
+    class: 'operator button',
+  },
+  {
+    keyCode: '',
+    key: '1',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '2',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '3',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '=',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '0',
+    id: '',
+    class: 'button',
+  },
+  {
+    keyCode: '',
+    key: '.',
+    id: '',
+    class: 'button',
+  },
+];
 
 const CalculatorButtons = () => {
-  return <button></button>;
+  return calcButtons.map((button) => {
+    return (
+      <button className={button.class} value={button.key}>
+        {button.key}
+      </button>
+    );
+  });
 };
 
 const Calculator = () => {
-  return <CalculatorButtons />;
+  return (
+    <div className='calc-keys'>
+      <CalculatorButtons />
+    </div>
+  );
 };
 
 export default Calculator;
