c0e4bf85a75dbe0fecb6132352edd9ece4240328
Zak Franczak <zakfranczak@gmail.com>
2023-07-21 12:47:47 -0500

    calculator styles set

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
+import Display from './components/Display';
 
 const App = () => {
   return (
     <div className='container'>
+      <Display />
       <Calculator />
     </div>
   );
---
+++ src/components/Display.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/display.css';
+
+const Display = () => {
+  return <div className='display'></div>;
+};
+
+export default Display;
