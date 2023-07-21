07a96a44fc3c64350a78130d5a9799774f60400e
Zak Franczak <zakfranczak@gmail.com>
2023-07-21 12:56:47 -0500

    added testing environment

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,111 +5,111 @@ const calcButtons = [
   {
     keyCode: '',
     key: 'AC',
-    id: '',
+    id: 'clear',
     class: 'all-clear',
   },
   {
     keyCode: '',
     key: '/',
-    id: '',
+    id: 'divide',
     class: 'operator button',
   },
   {
     keyCode: '',
     key: '*',
-    id: '',
+    id: 'multiply',
     class: 'operator button',
   },
   {
     keyCode: '',
     key: '7',
-    id: '',
+    id: 'seven',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '8',
-    id: '',
+    id: 'eight',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '9',
-    id: '',
+    id: 'nine',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '-',
-    id: '',
+    id: 'subtract',
     class: 'operator button',
   },
   {
     keyCode: '',
     key: '4',
-    id: '',
+    id: 'four',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '5',
-    id: '',
+    id: 'five',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '6',
-    id: '',
+    id: 'six',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '+',
-    id: '',
+    id: 'add',
     class: 'operator button',
   },
   {
     keyCode: '',
     key: '1',
-    id: '',
+    id: 'one',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '2',
-    id: '',
+    id: 'two',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '3',
-    id: '',
+    id: 'three',
     class: 'number button',
   },
   {
     keyCode: '',
     key: '=',
-    id: '',
+    id: 'equals',
     class: 'equals button',
   },
   {
     keyCode: '',
     key: '0',
-    id: '',
+    id: 'zero',
     class: 'number zero button',
   },
   {
     keyCode: '',
     key: '.',
-    id: '',
-    class: 'decimel button',
+    id: 'decimal',
+    class: 'decimal button',
   },
 ];
 
 const CalculatorButtons = () => {
   return calcButtons.map((button) => {
     return (
-      <button className={button.class} value={button.key}>
+      <button className={button.class} value={button.key} id={button.id}>
         {button.key}
       </button>
     );
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/display.css';
 
 const Display = () => {
-  return <div className='display'></div>;
+  return <div className='display' id='display'></div>;
 };
 
 export default Display;
