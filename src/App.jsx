f70526b78cc55c13c660c1418ca4c925891629c7
Zak Franczak <zakfranczak@gmail.com>
2023-07-21 10:22:52 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+const calcButtons = [
+  {
+    keyCode: '',
+    key: '',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '/',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '*',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '7',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '8',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '9',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '-',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '4',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '5',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '6',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '+',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '1',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '2',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '3',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '=',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '0',
+    id: '',
+  },
+  {
+    keyCode: '',
+    key: '.',
+    id: '',
+  },
+];
+
+const App = () => {
+  return <Calculator />;
+};
+
+export default App;
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CalculatorButtons = () => {
+  return <button></button>;
+};
+
+const Calculator = () => {
+  return <CalculatorButtons />;
+};
+
+export default Calculator;
