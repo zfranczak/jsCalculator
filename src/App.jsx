ee0a3df6b8a71460fc3a1ffa1052f626887a253b
Zak Franczak <zakfranczak@gmail.com>
2023-07-22 08:55:29 -0500

    button input working

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,22 @@ import Footer from './components/Footer';
 const App = () => {
   const [input, setInput] = useState('0');
   const [output, setOutput] = useState('');
+
+  const handleButtonClick = (value) => {
+    setInput((prevInput) => {
+      if (prevInput === '0' && value !== '.') {
+        return value; // If input is '0', replace it with the clicked button value (except for '.')
+      } else {
+        return prevInput + value; // Otherwise, append the clicked button value to the input
+      }
+    });
+  };
+
   return (
     <div>
       <div className='container'>
         <Display input={input} output={output} />
-        <Calculator />
+        <Calculator handleButtonClick={handleButtonClick} />
       </div>
       <Footer />
     </div>
